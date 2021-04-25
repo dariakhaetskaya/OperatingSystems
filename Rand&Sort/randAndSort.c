06fa05d45706eef22285a527f1a87715abb2a102
Daria <68292792+dariakhaetskaya@users.noreply.github.com>
2021-04-25 16:11:56 +0700

    Create randAndSort.c

---
+++ Rand&Sort/randAndSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <libgen.h>
+
+int main() {
+	srand(time(NULL));
+	FILE * fd[2];
+	if(p2open("sort", fd) == -1){
+		perror("p2open failed");
+		return -1;
+	} 
+
+	int currentNum;
+
+	printf("Generated array:\n");
+	for(int i = 0; i < 10; i ++){
+		for(int j = 0; j < 10; j++){
+			currentNum = rand() % 100;
+			fprintf(fd[0], "%d\n", currentNum);
+			printf("%3d ", currentNum); 
+		}
+		printf("\n");
+	}
+	if (pclose(fd[0]) == -1){
+		perror("pclose failed");
+		return -1;
+	}
+
+	printf("\n");
+
+	printf("Sorted array:\n");
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			currentNum = rand() % 100;
+			fscanf(fd[1], "%d", &currentNum);
+			printf("%3d ", currentNum);
+		}
+		printf("\n");
+	}
+
+	if (pclose(fd[1]) == -1){
+		perror("pclose failed");
+		return -1;
+	}
+	return 0;
+}
